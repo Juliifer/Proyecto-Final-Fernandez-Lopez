d37455f1164ef87f9b03548c8f1841c30dafb50e
Juli Fernandez <julyfernandezlopez@gmail.com>
2023-10-04 10:53:38 -0300

    detalles

--- appcoder/urls.py
+++ appcoder/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import inicio, Proyectos_Ley, Integrantes_proyect, Camara, Fecha_Inicio
 
 urlpatterns = [
-    path('index/',inicio), #esta era nuestra primer view
+    path('index2/',inicio), #esta era nuestra primer view
     path('proyectos_ley', Proyectos_Ley),
     path('integrantes_proyecto', Integrantes_proyect),
     path('camara', Camara),
