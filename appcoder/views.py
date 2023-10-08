b55abde8580463b4070eb1415fee2972634d7a6e
Juli Fernandez <julyfernandezlopez@gmail.com>
2023-10-08 18:10:55 -0300

    detalles nuevos

--- appcoder/views.py
+++ appcoder/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return render(request, 'AppCoder/index.html')
+    return render(request, 'AppCoder/inicio.html')
 
 def Proyectos_Ley(request):
     return render(request, 'AppCoder/Proyectos._Ley.html')
