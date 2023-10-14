637074ca56bf1f066f42a193792415e1eeda3710
Juli Fernandez <julyfernandezlopez@gmail.com>
2023-10-14 14:44:17 -0300

    cambio

--- appcoder/urls.py
+++ appcoder/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.inicio, name= 'Inicio'),
     path('proyectos_ley/', views.proyectos_Ley, name= 'Proyectos_Ley'),
-    path('integrantes_proyecto/', views.Integrantes_proyect,name= 'Integrantes_proyect' ),
-    path('camara/', views.Camara, name= 'Camara'),
+    path('integrantes_proyecto/', views.integrantes_proyect,name= 'Integrantes_proyect' ),
+    path('camara/', views.camara, name= 'Camara'),
     path('fecha_inicio/', views.Fecha_Inicio, name= 'Fecha_Inicio'),
     path('cursoFormulario', views.cursoFormulario, name='CursoFormulario'),
     path('integrantesFormulario', views.integrantesFormulario, name='IntegrantesFormulario'),
